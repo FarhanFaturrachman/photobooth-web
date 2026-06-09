23cb9481e5ad0de193008418ac15bfd5ef8aabbc
FarhanFaturrachman <farhanfaturrachman11@gmail.com>
2026-06-09 12:10:45 +0700

    Update script.js

--- script.js
+++ script.js
@@ -118,7 +118,7 @@ function loadFramesToContainer(containerId, className, isSidebar = false) {
     container.innerHTML = '';
     
     // Sesuaikan angka 10 dengan jumlah total file frame yang kamu punya
-    for(let i=1; i<=10; i++) {
+    for(let i=1; i<=5; i++) {
         const img = document.createElement('img');
         img.src = `frame/frame${i}.png`; 
         img.className = className;
